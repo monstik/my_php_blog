9ef0e5869907b1f81f7fbb7d3b004421de0cbad8
monstrik <55989179+monstik@users.noreply.github.com>
2019-11-03 15:35:56 +0200

    created site routes

---
+++ application/config/routes.php
@@ -0,0 +1,11 @@
+<?php
+return[
+    'account/login' => [
+        'controller' => 'account',
+        'action' => 'login'
+    ],
+    'account/register' =>[
+        'controller' => 'account',
+        'action' => 'register'
+    ],
+];
--- application/core/Route.php
+++ application/core/Route.php
@@ -3,5 +3,27 @@
 
 class Route
 {
+    protected $routes = [];
+    protected $params = [];
 
+    public function __construct()
+    {
+
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function match()
+    {
+
+    }
+
+    public function run()
+    {
+
+    }
 }
