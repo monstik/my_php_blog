f5d101a246581b89166b36c60405723922e08e8d
monstrik <55989179+monstik@users.noreply.github.com>
2019-11-04 23:08:03 +0200

    fixed styles

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
+
+require 'application/lib/Dev.php';
+
 use application\core\Router;
-require_once "application/lib/Dev.php";
-spl_autoload_register(function($class)
-{
-    $path = $class . ".php";
-    if (file_exists($path))
-    {
-        require_once $path;
+
+spl_autoload_register(function($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
     }
 });
 
+session_start();
 
-
-$router = new Router();
+$router = new Router;
 $router->run();
