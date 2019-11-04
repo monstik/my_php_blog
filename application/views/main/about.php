7a305afff7e3f258f48124cc5ef0528b0c9ba43e
monstrik <55989179+monstik@users.noreply.github.com>
2019-11-04 21:17:34 +0200

    createt new views for my blog

---
+++ application/views/main/about.php
@@ -0,0 +1,19 @@
+<header class="masthead" style="background-image: url('/public/images/about-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="page-heading">
+                    <h1>Я программист</h1>
+                    <span class="subheading">гавнокодю на  PHP</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <p>Как всегда, допишу завтра</p>
+        </div>
+    </div>
+</div>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,9 +1,35 @@
-<h1>Главная страница</h1>
-<div>
-    <?php
-    foreach ($news as $val) :?>
-        <h2><?php echo $val['title'] ?></h2>
-        <p><?php echo $val['description'] ?></p>
-    <?php endforeach;?>
-
+<header class="masthead" style="background-image: url('/public/images/home-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h1>Блог на PHP</h1>
+                    <span class="subheading">г-код</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <?php if (empty($list)): ?>
+                <p>Список постов пуст</p>
+            <?php else: ?>
+                <?php foreach ($list as $val): ?>
+                    <div class="post-preview">
+                        <a href="/post/<?php echo $val['id']; ?>">
+                            <h2 class="post-title"><?php echo htmlspecialchars($val['name'], ENT_QUOTES); ?></h2>
+                            <h5 class="post-subtitle"><?php echo htmlspecialchars($val['description'], ENT_QUOTES); ?></h5>
+                        </a>
+                        <p class="post-meta">Идентфикатор этого поста <?php echo $val['id']; ?></p>
+                    </div>
+                    <hr>
+                <?php endforeach; ?>
+                <div class="clearfix">
+                    <?php echo $pagination; ?>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
